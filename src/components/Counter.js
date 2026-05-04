6fcdd64ddfe1d8d3e17b57e18f1e294098b9e512
Marco Di Giulio <m.digiulio@jdk.it>
2026-05-04 17:54:43 +0200

    examples of redux for counter

---
+++ src/components/Counter.js
@@ -0,0 +1,35 @@
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
+
+const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const toggleCounterHandler = () => {};
+
+  const incrementHandler = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+
+  const increaseHandler = () => {
+    dispatch({ type: "INCREASE", amount: 5})
+  }
+  
+  const decrementHandler = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increment by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
