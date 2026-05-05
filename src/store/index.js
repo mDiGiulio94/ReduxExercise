2f60b8e30d3f94a8947266b3b754842ffbc28188
Marco Di Giulio <m.digiulio@jdk.it>
2026-05-05 18:28:47 +0200

    redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -33,55 +33,14 @@
 // export default store;
 
 // Redux con TOOLKIT
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./slice/counterSlice";
+import authReducer from "./slice/authSlice";
 
-const initialState = {
-  counter: 0,
-  showCounter: true,
-};
-
-const initialAuthState = {
-  isAuthenticated: false,
-};
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: initialState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload.amount;
-    },
-    toggle(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const authSlice = createSlice({
-  name: "authentication",
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
-
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 const store = configureStore({
   reducer: {
-    counter: counterSlice.reducer,
-    auth: authSlice.reducer,
+    counter: counterReducer,
+    auth: authReducer,
   },
 });
 
